7c4e25fc9a49cd83bca1fd3debaf74ed86f19407
moemenhafez <moemen.hafez777@gmail.com>
2026-05-20 00:11:34 +0300

    Connect global authentication provider to application root

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import AppRoutes from './routes/AppRoutes'
+import AuthProvider from '@/context/AuthProvider'
+
+import AppRoutes from '@/routes/AppRoutes'
 
 function App() {
-  return <AppRoutes />
+  return (
+    <AuthProvider>
+      <AppRoutes />
+    </AuthProvider>
+  )
 }
 
 export default App
